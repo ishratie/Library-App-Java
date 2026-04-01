3e71eb1ee46083113ad5cf90c6429cd75dbe06a4
Ilia <iliadanilov65@gmail.com>
2026-04-01 04:32:22 +0300

    new tests, one tests the borrowing system, and the other tests the status of the book

--- src/test/java/fi/jyu/ohj2/iliadanilo/kirjasto/project/model/KirjaTest.java
+++ src/test/java/fi/jyu/ohj2/iliadanilo/kirjasto/project/model/KirjaTest.java
@@ -2,8 +2,9 @@ package fi.jyu.ohj2.iliadanilo.kirjasto.project.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.LocalDate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class KirjaTest{
     @Test
     void test() {
@@ -16,4 +17,22 @@ public class KirjaTest{
         assertEquals("Tekijä", kirja.getTekija());
         assertEquals("1333", kirja.getIsbn());
     }
+
+    @Test
+    void testLisaaLainaus() {
+        Kirja kirja = new Kirja("Nimi", "Tekijä", "1333");
+        Lainaus lainaus = new Lainaus("Matti", LocalDate.now(), null, LocalDate.now().plusDays(7), kirja);
+        kirja.lisaaLainaus(lainaus);
+        assertEquals(1, kirja.getLainauksetNmr());
+    }
+
+    @Test
+    void testTilanne() {
+        Kirja kirja = new Kirja("Nimi", "Tekijä", "1333");
+        assertEquals("Saatavilla", kirja.getTilanne());
+        Lainaus lainaus = new Lainaus("Matti", LocalDate.now(), null, LocalDate.now().plusDays(7), kirja);
+        kirja.lisaaLainaus(lainaus);
+        assertEquals("Lainassa", kirja.getTilanne());
+    }
 }
+
